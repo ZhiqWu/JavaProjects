c37fe75489ca42552a1ca98cad25bb092d066b5d
ZhiqWu <1137994414@qq.com>
2022-10-25 20:31:16 +0800

    20221025

--- objPro/src/Sxtstu2.java
+++ objPro/src/Sxtstu2.java
@@ -3,7 +3,18 @@ public class Sxtstu2 {
     String sname;
     int age;
 
-    Sxtstu2(){//无参构造器。如果没有手动定义，系统自动添加。
+    Sxtstu2() {//无参构造器。如果没有手动定义，系统自动添加。
+    }
+
+    Sxtstu2(int id) {//重载
+        System.out.println(id);//就近原则
+        this.id = id;
+    }
+
+    Sxtstu2(int id, String name, int age) {
+        this.id = id;
+        this.sname = name;
+        this.age = age;
 
     }
 
