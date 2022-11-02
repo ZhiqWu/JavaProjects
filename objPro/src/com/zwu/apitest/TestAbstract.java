5490fb1487406b5e7ce9845e527687f021f29496
ZhiqWu <1137994414@qq.com>
2022-11-02 16:33:05 +0800

    20221102

--- objPro/src/com/zwu/apitest/TestAbstract.java
+++ objPro/src/com/zwu/apitest/TestAbstract.java
@@ -1,2 +1,35 @@
-package com.zwu.apitest;public class TestAbstract {
+package com.zwu.apitest;
+
+public class TestAbstract {
+    public static void main(String[] args) {
+        Dog a=new Dog();
+        a.shout();
+        a.seeDoor();
+    }
+}
+
+abstract class Animal{
+    private int id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    abstract public void shout();   //抽象方法
+
+    Animal(){}
+    public Animal(String name) {
+        this.name = name;
+    }
+}
+
+class Dog extends Animal{
+    //子类必须实现父类的抽象方法，否则编译错误
+    public void shout(){
+        System.out.println("汪汪汪！");
+    }
+    public void seeDoor(){
+        System.out.println("看门中！");
+    }
 }
--- objPro/src/com/zwu/testString/TestString01.java
+++ objPro/src/com/zwu/testString/TestString01.java
@@ -1,2 +1,13 @@
-package com.zwu.testString;public class TestString01 {
+package com.zwu.testString;
+
+public class TestString01 {
+    public static void main(String[] args) {
+        String str1 = new String("abcdefg");
+        String str2 = "abcdefg";
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str1 == str2);
+        //涉及到字符串比较的时候，用equals
+        System.out.println(str1.equals(str2));
+    }
 }
