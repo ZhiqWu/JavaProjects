d8ef1b197bc7291709f0122e92e4f54b47e9108f
ZhiqWu <1137994414@qq.com>
2022-11-03 23:08:31 +0800

    20221103

--- arrayPro/src/com/come/arrayTest/Test02.java
+++ arrayPro/src/com/come/arrayTest/Test02.java
@@ -1,2 +1,35 @@
-package com.come.arrayTest;public class Test02 {
+package com.come.arrayTest;
+
+public class Test02 {
+    public static void main(String[] args) {
+        Man[] mans=new Man[5];
+        mans={};
+    }
+}
+
+class Man{
+    private int id;
+    private int name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+    public Man(){}
+
+    public Man(int id, int name) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- objPro/src/com/zwu/TestArray/Test01.java
+++ objPro/src/com/zwu/TestArray/Test01.java
@@ -1,2 +1,12 @@
-package com.zwu.TestArray;public class Test01 {
+package com.zwu.TestArray;
+
+public class Test01 {
+    public static void main(String[] args) {
+        int[] s = null; //声明数组
+        s = new int[10];//给数组分配空间
+        for (int i = 0; i < 10; ++i) {
+            s[i] = i + 1;//给数组元素赋值；数组是对象，数组中的元素就是对象的属性。
+            System.out.println(s[i]);
+        }
+    }
 }
