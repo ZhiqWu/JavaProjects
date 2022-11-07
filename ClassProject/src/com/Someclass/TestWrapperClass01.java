09d3b4e73fea5b479a91c3220820bf418c97ec66
ZhiqWu <1137994414@qq.com>
2022-11-07 21:31:29 +0800

    20221107

--- ClassProject/src/com/Someclass/TestWrapperClass01.java
+++ ClassProject/src/com/Someclass/TestWrapperClass01.java
@@ -1,6 +1,6 @@
 package com.Someclass;
 
-public class TestWrapperClass01{
+public class TestWrapperClass01 {
     public static void main(String[] args) {
         //Integer i = new Integer(300); //基本数据类型转换为Integer对象，从java9开始废弃
         Integer a = Integer.valueOf(20); //官方推荐写法
@@ -17,6 +17,25 @@ public class TestWrapperClass01{
 
         //一些常用的常量
         System.out.println(Integer.MAX_VALUE);
+
+        //自动装箱
+        Integer i5 = 300;     //编译器帮你改成：Integer a = Integer.valueOf(300);
+
+        //自动拆箱
+        int i6 = a;           //编译器帮你改成：a.IntValue();
+
+        Integer i7 = null;    //编译器帮你改成：i7.IntValue();
+        //int i8 = i7;          //java.lang.NullPointerException:对象为null，我们调用了他的属性或方法
+
+        //包装类的缓存问题
+        Integer d1 = 4000;
+        Integer d2 = 4000;
+        //当数字在[-128,127]之间的时候，返回缓存数组中的某个元素。
+        Integer d3 = 123;
+        Integer d4 = 123;
+        System.out.println(d1 == d2); //false
+        System.out.println(d3 == d4); //true
+        System.out.println(d1.equals(d2));
 
 
     }
--- ClassProject/src/com/Someclass/TestWrapperClass02.java
+++ ClassProject/src/com/Someclass/TestWrapperClass02.java
@@ -1,2 +1,44 @@
-package com.Someclass;public class TestWrapperClass02 {
+package com.Someclass;
+
+/*
+ * 定义一个简单的包装类并不难
+ * */
+public class TestWrapperClass02 {
+    public static void main(String[] args) {
+        MyInteger m = MyInteger.valueOf(300);
+        System.out.println(m);
+
+    }
+}
+
+class MyInteger {
+    private int value;
+    private static MyInteger[] cache = new MyInteger[256];
+    public static final int Low = -128;
+    public static final int High = 127;
+
+
+    static {
+        //[-128,127]
+        for (int i = Low; i <= High; ++i) {
+            cache[i + 128] = new MyInteger(i);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.value + "";
+    }
+
+    public static MyInteger valueOf(int i) {
+        if (i >= Low && i <= High) {
+            return cache[i + 128];
+        }
+        return new MyInteger(i);
+    }
+
+    private MyInteger(int i) {
+        this.value = i;
+    }
+
 }
