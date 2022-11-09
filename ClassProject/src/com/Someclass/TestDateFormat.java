51776f728943ace9a75270d79d9d73bca783a4df
ZhiqWu <1137994414@qq.com>
2022-11-09 17:15:25 +0800

    20221109

--- ClassProject/src/com/Someclass/TestDateFormat.java
+++ ClassProject/src/com/Someclass/TestDateFormat.java
@@ -1,9 +1,14 @@
 package com.Someclass;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/*
+ * 测试时间对象和字符串的互相转化
+ * 使用：DateFormat、SimpleDateFormat
+ * */
 public class TestDate02 {
     public static void main(String[] args) throws ParseException {
         //new出SimpleDateFormat对象
@@ -17,5 +22,12 @@ public class TestDate02 {
 
         //将符合指定格式的字符串转成时间对象。字符串格式需要和指定格式一致。
         String time1 = "2007-10-7";
+        Date d1 = s2.parse(time1);
+        System.out.println(d1.getTime());
+
+        Date d2 = new Date();
+        DateFormat df2 = new SimpleDateFormat("今年第w周,今年的第D天");
+        System.out.println(df2.format(d2));
+
     }
 }
