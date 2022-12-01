87173829797f2b5646ba40bb6f1217625e42c63b
ZhiqWu <1137994414@qq.com>
2022-12-01 20:15:08 +0800

    20221201

---
+++ CollectionPro/src/com/zwu/MyCollection/MyLinkedList02.java
@@ -0,0 +1,82 @@
+package com.zwu.MyCollection;
+
+
+/*
+ * 定义一个链表
+ * */
+public class MyLinkedList01 {
+    private Node first;
+    private Node last;
+    private int size;
+
+
+    //[]
+    //["a","b","c"]
+    public void add(Object obj) {
+        Node node = new Node(obj);
+        if (first == null) {
+//            node.previous = null;
+//            node.next=null;
+            first = node;
+            last = node;
+        } else {
+            node.previous = last;
+            node.next = null;
+            last.next = node;
+            last = node;
+        }
+        size++;
+    }
+
+    //[a,b,c,d,e,f] c 2
+    public Object get(int index) {
+
+        if (index < 0 || index > size - 1) {
+            throw new RuntimeException("索引数字不合法：" + index);
+        }
+        Node temp=null;
+        if (index < (size >> 1)) {
+            temp = first;
+            for (int i = 0; i < index; ++i) {
+                temp = temp.next;
+            }
+        } else {
+            temp = last;
+            for (int i = size-1; i > index; --i) {
+                temp = temp.previous;
+            }
+        }
+
+
+        return temp.element;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+
+        //[a,b,c] first=a,last=c
+        //a,b,c
+        Node temp = first;
+        while (temp != null) {
+            sb.append(temp.element + ",");
+            temp = temp.next;
+        }
+
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList01 list = new MyLinkedList01();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        list.add("f");
+        System.out.println(list);
+        System.out.println(list.get(5));
+    }
+
+}
---
+++ CollectionPro/src/com/zwu/collection/TestVector.java
@@ -0,0 +1,13 @@
+package com.zwu.MyCollection;
+
+import java.util.List;
+import java.util.Vector;
+
+/*
+* 测试vector
+* */
+public class TestVector {
+    public static void main(String[] args) {
+        List<String> list=new Vector<>();
+    }
+}
