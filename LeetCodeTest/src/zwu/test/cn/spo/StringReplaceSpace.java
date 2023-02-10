d6066313f6ad0f89e632c03ecd1c8b72b30cf6c7
ZhiqWu <1137994414@qq.com>
2023-02-10 14:43:23 +0800

    2023/2/10

--- LeetCodeTest/src/zwu/test/cn/spo/StringReplaceSpace.java
+++ LeetCodeTest/src/zwu/test/cn/spo/StringReplaceSpace.java
@@ -1,2 +1,14 @@
-package zwu.test.cn.spo;public class StringReplaceSpace {
+package zwu.test.cn.spo;
+
+public class StringReplaceSpace {
+    public String replaceSpace(String s) {
+        StringBuilder str=new StringBuilder(s);
+        while(str.indexOf(" ")!=-1){
+            int pos=str.indexOf(" ");
+            str.deleteCharAt(pos);
+            str.insert(pos,"%20");
+        }
+        return str.toString();
+
+    }
 }
--- LeetCodeTest/src/zwu/test/cn/spo/StringReverseLeftWords.java
+++ LeetCodeTest/src/zwu/test/cn/spo/StringReverseLeftWords.java
@@ -1,2 +1,12 @@
-package zwu.test.cn.spo;public class StringReverseLeftWords {
+package zwu.test.cn.spo;
+
+public class StringReverseLeftWords {
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder str = new StringBuilder(s);
+        StringBuilder str1 = new StringBuilder(str.substring(0, n - 1));
+        str.delete(0, n - 1);
+        str.append(str1);
+        return str.toString();
+
+    }
 }
