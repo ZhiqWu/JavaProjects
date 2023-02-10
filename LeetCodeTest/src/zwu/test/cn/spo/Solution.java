f0f2b833217540069b1acad7f3dc800dde0b47de
ZhiqWu <1137994414@qq.com>
2023-02-10 18:23:36 +0800

    20230210

--- LeetCodeTest/src/zwu/test/cn/spo/Solution.java
+++ LeetCodeTest/src/zwu/test/cn/spo/Solution.java
@@ -1,2 +1,20 @@
-package zwu.test.cn.spo;public class Solution {
+package zwu.test.cn.spo;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        int length = nums.length;
+        for (int i = 0; i < length; ++i) {
+            for (int j = i + 1; j < length; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+
+    }
+
 }
