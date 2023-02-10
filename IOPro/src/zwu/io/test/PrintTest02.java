1c3882286e3b23808907b77852cdddb57c609c14
ZhiqWu <1137994414@qq.com>
2023-02-10 09:32:27 +0800

    20230210

--- IOPro/src/zwu/io/test/PrintTest02.java
+++ IOPro/src/zwu/io/test/PrintTest02.java
@@ -3,25 +3,15 @@ package zwu.io.test;
 import java.io.*;
 
 /*
- * 打印流 PrintStream
+ * 打印流 PrintWriter
  * */
-public class PrintTest01 {
+public class PrintTest02 {
     public static void main(String[] args) throws FileNotFoundException {
-        PrintStream ps = System.out;
-        ps.println("打印流");
-        ps.println(true);
 
-        ps = new PrintStream(new BufferedOutputStream(new FileOutputStream("print.txt")), true);
-        ps.println("打印流");
-        ps.println(true);
-        ps.close();
 
-        //重定向输出端
-        System.setOut(ps);
-        System.out.println("改变");
-
-        //重定向回控制台
-        System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(FileDescriptor.out)), true));
-        System.out.println("我回来了！");
+        PrintWriter pw = new PrintWriter(new BufferedOutputStream(new FileOutputStream("print.txt")), true);
+        pw.println("打印流");
+        pw.println(true);
+        pw.close();
     }
 }
